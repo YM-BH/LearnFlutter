3098c7ccffd13f995f5c83dddf1c3338cfc6ad33
白晗 <baih_1989@163.com>
2022-11-07 20:05:30 +0800

    学习flutter

---
+++ Projects/first_flutter_demo/android/app/src/main/kotlin/cn/first_flutter_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package cn.first_flutter_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Projects/learn_widgets/lib/main.dart
+++ Projects/learn_widgets/lib/main.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,50 +6,53 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("单选开关和复选框"),
-        ),
-        body: SwitchAndCheckbox()
-      ),
+          appBar: AppBar(
+            title: const Text("TextField组件"),
+          ),
+          body: YMTextField()),
     );
   }
 }
 
-class SwitchAndCheckbox extends StatefulWidget {
-  SwitchAndCheckbox({super.key});
+class YMTextField extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return SwitchAndCheckboxState();
+    return YMTextFieldState();
   }
 }
 
-class SwitchAndCheckboxState extends State<SwitchAndCheckbox> {
-  bool switchSelected = false;
-  bool checkboxSelected = false;
+class YMTextFieldState extends State<YMTextField> {
+  TextEditingController usernameController = TextEditingController();
+
+  @override
+  void initState() {
+    usernameController.addListener(() {
+      print(usernameController.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: [
-        Switch(
-          value: switchSelected,
-          onChanged: (newValue) {
-            setState(() {
-              switchSelected = newValue;
-            });
-          },
+        TextField(
+          autofocus: true,
+          controller: usernameController,
+          decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "请输入用户名或邮箱",
+              prefixIcon: Icon(Icons.person)),
         ),
-        Checkbox(
-            value: checkboxSelected,
-            onChanged: (newValue) {
-              setState(() {
-                checkboxSelected = newValue!;
-              });
-            })
+        TextField(
+          decoration: InputDecoration(
+              labelText: "密码", hintText: "请输入密码", prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+        )
       ],
     );
   }
