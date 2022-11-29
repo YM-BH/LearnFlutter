99d489af515d698bd34c8ea1f2dd728029643129
白晗 <baih_1989@163.com>
2022-11-29 14:37:30 +0800

    学习layout布局

--- Projects/learn_widgets/lib/main.dart
+++ Projects/learn_widgets/lib/main.dart
@@ -1,59 +1,67 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  // debugPaintSizeEnabled = true;
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("TextField组件"),
+        appBar: AppBar(
+          title: const Text(
+            "row_column",
+            style: TextStyle(
+              color: Colors.orange,
+              fontSize: 30
+            ),
           ),
-          body: YMTextField()),
-    );
-  }
-}
-
-class YMTextField extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return YMTextFieldState();
-  }
-}
-
-class YMTextFieldState extends State<YMTextField> {
-  TextEditingController usernameController = TextEditingController();
-
-  @override
-  void initState() {
-    usernameController.addListener(() {
-      print(usernameController.text);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          autofocus: true,
-          controller: usernameController,
-          decoration: InputDecoration(
-              labelText: "用户名",
-              hintText: "请输入用户名或邮箱",
-              prefixIcon: Icon(Icons.person)),
         ),
-        TextField(
-          decoration: InputDecoration(
-              labelText: "密码", hintText: "请输入密码", prefixIcon: Icon(Icons.lock)),
-          obscureText: true,
-        )
-      ],
+        body: Center(
+          child: buildRow(),
+        ),
+      ),
     );
   }
+}
+
+Widget buildRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Image.asset(
+        "assets/images/pic1.jpg"
+      ),
+      Image.asset(
+        "assets/images/pic2.jpg"
+      ),
+      Image.asset(
+        "assets/images/pic3.jpg"
+      )
+    ],
+  );
+}
+
+Widget buildColumn() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Image.asset(
+        "assets/images/pic1.jpg"
+      ),
+      Image.asset(
+        "assets/images/pic2.jpg"
+      ),
+      Image.asset(
+        "assets/images/pic3.jpg"
+      )
+    ],
+  );
 }
